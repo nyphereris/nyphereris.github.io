f6dc94f3f50896e95a3590c17d27652b3f155356
nyphereris <nyphereris@gmail.com>
2025-06-03 00:41:03 -0400

    Deploy site manually to gh-pages branch

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,22 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1748925646',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
