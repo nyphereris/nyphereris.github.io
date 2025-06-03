0766aeab7ca064fe15c02a0f5c9a642da70db300
nyphereris <nyphereris@gmail.com>
2025-06-03 02:11:56 -0400

    Deploy static site from localhost build

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,22 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1748931116',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
