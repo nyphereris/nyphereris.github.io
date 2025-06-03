87bdfeb49cf37140a1705585a0d28ee617027a55
nyphereris <nyphereris@gmail.com>
2025-06-03 01:04:01 -0400

    Add files via upload

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,22 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1748926536',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
