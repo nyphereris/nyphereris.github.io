57b2480021fdbd33ca2f2f2f38213f43596b7953
nyphereris <nyphereris@gmail.com>
2025-06-03 01:06:53 -0400

    Trigger rebuild to include uploaded posts

---
+++ README.md
@@ -0,0 +1,1 @@
+ # or touch trigger.txt
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748925646',resources: [
+    cacheName: 'chirpy-1748926536',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
